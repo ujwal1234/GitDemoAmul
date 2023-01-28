70b81a378428830b110965db77197a2dc7971fc2
ujwal1234 <30384817+ujwal1234@users.noreply.github.com>
2023-01-28 11:49:01 +0530

    This is first commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is demo java class");
+    }
+
+
+
+}
+
+
+
---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,7 @@
+public class DemoTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is the Demo Test Class");
+    }
+}
